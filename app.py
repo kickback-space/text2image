ac63092d95f93a4b22dc2e74035ce5b6229d26ad
Pascal Oseko <pascal@kickback.space>
2024-03-21 13:28:27 +0000

    fix: cuda issue
    - added better error handling
    - replaced cuda 11.8 to 12.1 which is compatible with the one in the VM
    - added logs and remove commands

--- app.py
+++ app.py
@@ -1,19 +1,30 @@
+from io import BytesIO
 from flask import Flask, jsonify, request
 from diffusers import StableDiffusionXLPipeline
 import torch
+import base64
 
 app = Flask(__name__)
 
 
 @app.route('/api/v1/text-to-image/', methods=['GET', 'POST'])
 def text_to_image():
-    pipe = StableDiffusionXLPipeline.from_pretrained("etri-vilab/koala-700m-llava-cap", torch_dtype=torch.float16)
-    pipe = pipe.to("cuda")
-    if request.method == 'POST':
-        prompt = request.get_json()['prompt']
-        image = pipe(prompt=prompt, width=1024, height=1024).images[0]
-        return jsonify({"image": image})
-    return {}
+    try:
+        pipe = StableDiffusionXLPipeline.from_pretrained("etri-vilab/koala-700m-llava-cap", torch_dtype=torch.float16)
+        pipe = pipe.to("cuda")
+        if request.method == 'POST':
+            prompt = request.get_json()['prompt']
+            image = pipe(prompt=prompt, width=1024, height=1024).images[0]
+
+            # Convert image to base64 string
+            buffered = BytesIO()
+            image.save(buffered, format="JPEG")
+            img_str = base64.b64encode(buffered.getvalue()).decode('utf-8')
+            return jsonify({"image": img_str})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+    return jsonify({"message": "Method not allowed"}), 405
 
 
 if __name__ == '__main__':
--- makefile
+++ makefile
@@ -4,4 +4,12 @@ build:
 
 .PHONY: run
 run:
-	docker run -p 5000:5000 text2image
+	docker run --name text_2_image --gpus all -d -p 5000:5000 text2image
+
+.PHONY: logs
+logs:
+	docker logs -f text_2_image
+
+.PHONY: remove
+remove:
+	docker stop $$(docker ps -a --filter ancestor=text2image -q) && docker rm $$(docker ps -a --filter ancestor=text2image -q)
