00e420b7db1d3504ee54f6cbcdd354469d9a9cec
Pascal Oseko <pascal@kickback.space>
2024-03-25 16:45:21 +0000

    add locust + dependencies

---
+++ locustfile.py
@@ -0,0 +1,11 @@
+from locust import HttpUser, task, between
+
+class MockUser(HttpUser):
+    wait_time = between(45, 60)
+
+    @task
+    def send_request(self):
+        data = {
+            "prompt": "cookie"
+        }
+        self.client.post("/api/v1/text-to-image/", json=data)
