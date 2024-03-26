3acb6da33040df6f4a65ab6f418df65c8d7071c1
Pascal Oseko <pascal@kickback.space>
2024-03-26 12:41:11 +0000

    add more kwargs

--- app.py
+++ app.py
@@ -21,28 +21,42 @@ def load_pipeline():
         # pipe.vae = AutoencoderTiny.from_pretrained("madebyollin/taesdxl", torch_dtype=torch.float16)
         pipe.to("cuda")
 
-def process_image(prompt: str):
+def process_image(**kwargs):
     load_pipeline()
-    image = pipe(prompt=prompt, width=1024, height=1024).images[0]
+    images = pipe(**kwargs).images
 
     torch.cuda.empty_cache()
-    return image
+    return images
 
 @app.route('/api/v1/text-to-image/', methods=['POST'])
 def text_to_image():
     try:
         if request.method == 'POST':
-            prompt = request.get_json()['prompt']
+            data = request.get_json()
+            prompt = data.get('prompt')
+            negative_prompt = data.get('negative_prompt', '')
+            num_inference_steps = data.get('num_inference_steps', 41)
+            width = data.get('width', 1024)
+            height = data.get('height', 1024)
             start = time.time() 
-            image = process_image(prompt)
+            images = process_image(
+                prompt=prompt,
+                negative_prompt=negative_prompt,
+                num_inference_steps=num_inference_steps,
+                width=width,
+                height=height
+                )
             end = time.time()
-            print("ELAPSED TIME LOADING PROCESSING IMAGE", end - start)
+            print("ELAPSED TIME LOADING IMAGE", end - start)
 
-            # Convert image to base64 string 
-            buffered = BytesIO()
-            image.save(buffered, format="JPEG")
-            img_str = base64.b64encode(buffered.getvalue()).decode('utf-8')
-            return jsonify({"image": img_str})
+            # Convert image to base64 string
+            img_strs = []
+            for image in images:
+                buffered = BytesIO()
+                image.save(buffered, format="JPEG")
+                img_str = base64.b64encode(buffered.getvalue()).decode('utf-8')
+                img_strs.append(img_str)
+            return jsonify({"images": img_strs})
     except Exception as e:
         print("ERROR", str(e))
         return jsonify({"error": str(e)}), 500
