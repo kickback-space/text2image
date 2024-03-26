b0e0daf247d2cee425c7cc7e41ebc12030171890
Pascal Oseko <pascal@kickback.space>
2024-03-26 18:52:18 +0000

    add: multi thread for concurrent request

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 from io import BytesIO
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 from diffusers import StableDiffusionXLPipeline
 import torch
 import base64
 import time
+from threading import Lock
 
 app = Flask(__name__)
+CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 pipe = None
-
+lock = Lock()
 
 def load_pipeline():
     global pipe
@@ -17,51 +21,47 @@ def load_pipeline():
             "etri-vilab/koala-700m-llava-cap",
             torch_dtype=torch.float16
         )
-        # With this setup, we reduce the memory requirement to 15.6GB while reducing the inference latency at the same time.
-        # pipe.vae = AutoencoderTiny.from_pretrained("madebyollin/taesdxl", torch_dtype=torch.float16)
         pipe.to("cuda")
-
-def process_image(**kwargs):
-    load_pipeline()
-    images = pipe(**kwargs).images
-
-    torch.cuda.empty_cache()
-    return images
 
 @app.route('/api/v1/text-to-image/', methods=['POST'])
 def text_to_image():
+    data = request.get_json()
+    if not data:
+        return jsonify({"error": "No data provided"}), 400
+
+    prompt = data.get('prompt')
+    negative_prompt = data.get('negative_prompt', '')
+    num_inference_steps = data.get('num_inference_steps', '41')
+    width = data.get('width', '1024')
+    height = data.get('height', '1024')
+
     try:
-        if request.method == 'POST':
-            data = request.get_json()
-            prompt = data.get('prompt')
-            negative_prompt = data.get('negative_prompt', '')
-            num_inference_steps = data.get('num_inference_steps', 41)
-            width = data.get('width', 1024)
-            height = data.get('height', 1024)
+        with lock:
+            load_pipeline()
             start = time.time() 
-            images = process_image(
+            images = pipe(
                 prompt=prompt,
                 negative_prompt=negative_prompt,
-                num_inference_steps=num_inference_steps,
-                width=width,
-                height=height
-                )
+                num_inference_steps=int(num_inference_steps),
+                width=int(width),
+                height=int(height)
+            ).images[0]
+            torch.cuda.empty_cache()
             end = time.time()
             print("ELAPSED TIME LOADING IMAGE", end - start)
 
             # Convert image to base64 string
-            img_strs = []
-            for image in images:
-                buffered = BytesIO()
-                image.save(buffered, format="JPEG")
-                img_str = base64.b64encode(buffered.getvalue()).decode('utf-8')
-                img_strs.append(img_str)
-            return jsonify({"images": img_strs})
+            buffered = BytesIO()
+            images.save(buffered, format="JPEG")
+            img_str = base64.b64encode(buffered.getvalue()).decode('utf-8')
+            return jsonify({
+                "data": {
+                "status": "success",
+                "output": img_str,
+            }})
     except Exception as e:
         print("ERROR", str(e))
         return jsonify({"error": str(e)}), 500
-    
-    return jsonify({"message": "Method not allowed"}), 405
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, threaded=True)
